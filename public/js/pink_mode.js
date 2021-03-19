6baded98594587142f1af1c7bfb16068d3983bf6
trinh Minh Chien <trinhminhchien110899@gmail.com>
2021-03-19 16:36:35 +0700

    add 2 more cyan and pink theme for user

---
+++ public/js/pink_mode.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // $('.text-black').addClass('text-light').removeClass('text-black');
+    // $('.bg-light,.bg-white').addClass('bg-secondary').removeClass('bg-light bg-white');
+    // $('.dropdown-menu.dropdown-menu-right').css('background-color', '#444950');
+    $('.bg-light').addClass('bg-success').removeClass('bg-light');
+})
