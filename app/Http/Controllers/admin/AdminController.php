26f3e6d585b4fbdd570ee6cbfdfd61f1c542c4c4
trinh Minh Chien <trinhminhchien110899@gmail.com>
2021-03-26 11:21:13 +0700

    add facebook message chat box

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -9,7 +9,7 @@ use App\Subject;
 class AdminController extends Controller
 {
     public function __construct(){
-		$this->middleware('auth:admin');;
+		$this->middleware('auth:admin');
 	}
 
 	public function index(Request $request){
